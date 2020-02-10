7f7cbcd6a2a30dcb355051e1e09f2496fa9710e8
[Armando Samperio] <armando.samperio@mavs.uta.edu>
2020-02-09 22:06:40 -0600

    Updates

---
+++ precache-manifest.46104b46c0126aa6bdc84dd52a7d7253.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86053de1901090abadc147dacdca5553",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2e7dcdc8154aa54d0d9",
+    "url": "/static/css/main.2e7ca379.chunk.css"
+  },
+  {
+    "revision": "64d4b2999b79243ab6bf",
+    "url": "/static/js/2.4aa0814e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.4aa0814e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e7dcdc8154aa54d0d9",
+    "url": "/static/js/main.9c09f08f.chunk.js"
+  },
+  {
+    "revision": "f72d27fe65576b9d61b9",
+    "url": "/static/js/runtime-main.0504ea47.js"
+  }
+]);
