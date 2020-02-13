e9f5b7999cf7e1bc7fc9a55e8b4f812a5e07cd15
Armando Samperio <armandosamperiomcchs@Armandos-MacBook-Pro.local>
2020-02-12 19:11:59 -0600

    updated @media for iphone x/ipad/ipadpro/galaxy/pixel 2

--- src/pages/EventsPage/EventsPage.jsx
+++ src/pages/EventsPage/EventsPage.jsx
@@ -12,8 +12,8 @@ class EventsPage extends Component {
                     <div className='card'>
                         <h1>Weekly Meetings</h1>
                         <img src='../images/hotel_one.jpg' className='card-img-top' alt='weekly meeting'></img>
-                        <h5 class='card-title'>Card Title</h5>
-                        <p class='card-text'>Location/Time/Date</p>
+                        <h5 className='card-title'>Card Title</h5>
+                        <p className='card-text'>Location/Time/Date</p>
                         <a href='https://www.google.com/maps/place/Holiday+Inn+Express+%26+Suites+Fort+Worth+West/@32.7113984,-97.4752189,13z/data=!4m11!1m2!2m1!1shotls!3m7!1s0x864e0cc44c8e6f5f:0x9edf19cb28711282!5m2!4m1!1i2!8m2!3d32.7360836!4d-97.4522436' className='btn btn-primary'>Link</a>
                     </div>
                 </div>
@@ -21,8 +21,8 @@ class EventsPage extends Component {
                     <div className='card'>
                         <h1>Seminars</h1>
                         <img src='../images/hotel_two.jpg' className='card-img-top' alt='weekly meeting'></img>
-                        <h5 class='card-title'>Card Title</h5>
-                        <p class='card-text'>Location/Time/Date</p>
+                        <h5 className='card-title'>Card Title</h5>
+                        <p className='card-text'>Location/Time/Date</p>
                         <a href='https://www.google.com/maps/place/Courtyard+by+Marriott+Fort+Worth+I-30+West+Near+NAS+JRB/@32.7113984,-97.4752189,13z/data=!4m11!1m2!2m1!1shotls!3m7!1s0x864e7331d3180c7f:0xfac7887884acaf3e!5m2!4m1!1i2!8m2!3d32.735478!4d-97.4300838' className='btn btn-primary'>Link</a>
                     </div>
                 </div>
@@ -30,8 +30,8 @@ class EventsPage extends Component {
                     <div className='card'>
                         <h1>Conventions</h1>
                         <img src='../images/hotel_three.jpg' className='card-img-top' alt='weekly meeting'></img>
-                        <h5 class='card-title'>Card Title</h5>
-                        <p class='card-text'>Location/Time/Date</p>
+                        <h5 className='card-title'>Card Title</h5>
+                        <p className='card-text'>Location/Time/Date</p>
                         <a href='https://www.google.com/' className='btn btn-primary'>Link</a>
                     </div>
                 </div>
